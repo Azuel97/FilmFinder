22ef2dc60596e38f2d3a149893d1c189ae59127a
Azuel97 <azuel@hotmail.it>
2020-04-10 15:36:53 +0200

    RecyclerView

---
+++ app/src/main/java/com/example/moviefinder/adapters/FilmAdapterRecycler.java
@@ -0,0 +1,97 @@
+package com.example.moviefinder.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.moviefinder.DetailMovieActivity;
+import com.example.moviefinder.R;
+import com.example.moviefinder.data.models.Film;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilmAdapterRecycler extends RecyclerView.Adapter<FilmAdapterRecycler.MyViewHolder> {
+
+    private Context context;
+    private List<Film> films = new ArrayList<>();
+    private OnFilmListener  mOnFilmListener;
+
+    public FilmAdapterRecycler(Context context, OnFilmListener onFilmListener) {
+        this.context = context;
+        this.mOnFilmListener = onFilmListener;
+    }
+
+    public void setFilms(List<Film> films) {
+        this.films = films;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        LayoutInflater mInflater = LayoutInflater.from(context);
+        view = mInflater.inflate(R.layout.film_cell,parent,false);
+
+        return new MyViewHolder(view, mOnFilmListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        final Film currentFilm = films.get(position);
+
+        // Glide for image
+        Glide.with(context)
+                .load("https://image.tmdb.org/t/p/w500/"+currentFilm.getPosterPath())
+                .into(holder.image);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context,DetailMovieActivity.class);
+                intent.putExtra("Title",currentFilm.getTitle());
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return films.size();
+    }
+
+
+
+    // Classe ViewHolder
+    public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        ImageView image;
+        OnFilmListener onFilmListener;
+
+        public MyViewHolder(@NonNull View itemView, OnFilmListener onFilmListener ) {
+            super(itemView);
+            this.onFilmListener = onFilmListener;
+            image = itemView.findViewById(R.id.imageFilm);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            onFilmListener.onFilmCLick(getAdapterPosition());
+        }
+    }
+
+
+    // Interface per il clickListener
+    public interface OnFilmListener {
+        void onFilmCLick(int position );
+    }
+}
